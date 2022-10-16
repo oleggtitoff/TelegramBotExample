1b226cd816fcc338e8ba088b57140416e600b8ae
Oleg Titov <oleggtitoff@gmail.com>
2022-10-16 14:49:43 +0300

    Register the Bot and make it repeat the user's message

--- src/main/java/ua/olehtitov/planner/SimpleEchoBot.java
+++ src/main/java/ua/olehtitov/planner/SimpleEchoBot.java
@@ -1,18 +1,44 @@
 package ua.olehtitov.planner;
 
+import lombok.Setter;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Setter
 @Component
 @RequiredArgsConstructor
 public class SimpleEchoBot extends TelegramLongPollingBot {
 
     private final BotProperties botProperties;
+    Logger log;
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String textFromUser = update.getMessage().getText();
+
+            Long userId = update.getMessage().getChatId();
+            String userFirstName = update.getMessage().getFrom().getFirstName();
+
+            log.info("[{}, {}] : {}", userId, userFirstName, textFromUser);
+
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(userId.toString())
+                    .text("Hello, I've received your text: " + textFromUser)
+                    .build();
+            try {
+                this.sendApiMethod(sendMessage);
+            } catch (TelegramApiException e) {
+                log.error("Exception when sending message: ", e);
+            }
+        } else {
+            log.warn("Unexpected update from user");
+        }
     }
 
     @Override
