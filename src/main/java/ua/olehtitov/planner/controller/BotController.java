c504cbafebf217f4f3517cd6340681f431977d8f
Oleg Titov <oleggtitoff@gmail.com>
2022-10-31 19:10:53 +0200

    Don't send the same message back to user. Refactor onUpdateReceived into few methods.

--- src/main/java/ua/olehtitov/planner/controller/BotController.java
+++ src/main/java/ua/olehtitov/planner/controller/BotController.java
@@ -5,9 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 @Setter
@@ -19,26 +18,23 @@ public class BotController extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String textFromUser = update.getMessage().getText();
-
-            Long userId = update.getMessage().getChatId();
-            String userFirstName = update.getMessage().getFrom().getFirstName();
-
-            log.info("[{}, {}] : {}", userId, userFirstName, textFromUser);
-
-            SendMessage sendMessage = SendMessage.builder()
-                    .chatId(userId.toString())
-                    .text("Hello, I've received your text: " + textFromUser)
-                    .build();
-            try {
-                this.sendApiMethod(sendMessage);
-            } catch (TelegramApiException e) {
-                log.error("Exception when sending message: ", e);
-            }
+        if (hasMessageWithText(update)) {
+            logMessage(update.getMessage());
         } else {
             log.warn("Unexpected update from user");
         }
+    }
+
+    private boolean hasMessageWithText(Update update) {
+        return update.hasMessage() && update.getMessage().hasText();
+    }
+
+    private void logMessage(Message message) {
+        Long chatId = message.getChatId();
+        String userFirstName = message.getFrom().getFirstName();
+        String text = message.getText();
+
+        log.info("[{}, {}] : {}", chatId, userFirstName, text);
     }
 
     @Override
